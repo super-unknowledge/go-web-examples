4a838554af783b83d82e0d360aa8e063f2939c6f
super-unknowledge <64061479+super-unknowledge@users.noreply.github.com>
2025-02-11 17:47:52 +0800

    feat: Add create and delete functions

--- 04-mysql/main.go
+++ 04-mysql/main.go
@@ -4,20 +4,14 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"time"
 
-	"github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	// Capture connection properties
-	cfg := mysql.Config{
-		User:   "root",
-		Passwd: "root",
-		Net:    "tcp",
-		Addr:   "127.0.0.1:3306",
-	}
 	// Get a database handle
-	db, err := sql.Open("mysql", cfg.FormatDSN())
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/test")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -27,4 +21,43 @@ func main() {
 		log.Fatal(pingErr)
 	}
 	fmt.Println("Connected!")
+
+	{ // Create a new table
+		query := `
+			CREATE TABLE users (
+				id INT AUTO_INCREMENT,
+				username TEXT NOT NULL,
+				password TEXT NOT NULL,
+				created_at DATETIME,
+				PRIMARY KEY (id)
+			);`
+ 
+		// Executes the SQL query in our database.
+		if _, err := db.Exec(query); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	{ // Insert a new user
+		username := "johndoe"
+		password := "secret"
+		createdAt := time.Now()
+
+		// Returns info about the last inserted id
+		result, err := db.Exec(`INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)`, username, password, createdAt)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		id, err := result.LastInsertId()
+		fmt.Println(id)
+	}
+
+	{ // Delete user from the table
+		_, err := db.Exec(`DELETE FROM users WHERE id = ?`, 1)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	// TODO: move CRUD functions outside main function
 }
