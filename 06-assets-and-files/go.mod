531bdb49dcae9ff175f0f345e4b3989458b11cfa
super-unknowledge <64061479+super-unknowledge@users.noreply.github.com>
2025-02-27 16:10:14 +0800

    Serve static file

---
+++ 06-assets-and-files/go.mod
@@ -0,0 +1,3 @@
+module github.com/super-unknowledge/06-assets-and-files
+
+go 1.23.4
---
+++ 06-assets-and-files/static-files.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func main() {
+	fs := http.FileServer(http.Dir("assets/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":8080", nil)
+}
+
+// curl -s http://localhost:8080/static/css/styles.css
+// to access static file
